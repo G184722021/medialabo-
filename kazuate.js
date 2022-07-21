4f63af92971226f65621d7921ba3f8d74c038e07
G184722021 <218472@st.takushoku-u.ac.jp>
2022-07-21 15:25:46 +0900

    j

--- kazuate.js
+++ kazuate.js
@@ -7,14 +7,18 @@ let kaisu = 0;
 
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
-hantei();
+//hantei();
 //hantei();
 //hantei();
 //hantei();
 
+let b = document.querySelector('#print');
+b.addEventListener('click', greeting);
+
 // ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() {
-    let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
+function greeting() {
+    let i = document.querySelector('input[name="yoso"]');
+	let yoso = i.value;      // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
---
+++ project.js
@@ -0,0 +1,40 @@
+let b = document.querySelector('#print');
+b.addEventListener('click', sendRequest);
+
+
+let id = 360630;
+
+var select = document.getElementById('city-select');
+select.onchange = function(){
+    alert(this.value);
+}
+
+function sendRequest() {
+	let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + id + '.json';
+
+	axios.get(url)
+		.then(showResult)
+		.catch(showError)
+		.then(finish);
+}
+
+function showResult(resp) {
+	// サーバから送られてきたデータを出力
+	let data = resp.data;
+
+	// data が文字列型なら，オブジェクトに変換する
+	if (typeof data === 'string') {
+		data = JSON.parse(data);
+	}
+
+	// data をコンソールに出力
+	console.log(data);
+}
+function showError(err) {
+	console.log(err);
+}	
+
+// 通信の最後にいつも実行する処理
+function finish() {
+	console.log('Ajax 通信が終わりました');
+}
