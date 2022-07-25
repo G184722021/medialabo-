f9f3158ab8337125189a83824b9a131e6e319532
G184722021 <86222549+G184722021@users.noreply.github.com>
2022-07-26 01:10:31 +0900

    t

--- project.js
+++ project.js
@@ -1,16 +1,12 @@
 let b = document.querySelector('#sendRequest');
 b.addEventListener('click', sendRequest);
-
-
-let id = 360630;
-
-var select = document.getElementById('city');
-
-select.onchange = function(){
-  alert(this.value);
-}
+let c = document.querySelector('#reset');
+c.addEventListener('click', reset);
 
 function sendRequest() {
+	var city = document.getElementById("city");
+	let id = city.value;
+
 	let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + id + '.json';
 
 	axios.get(url)
@@ -24,12 +20,16 @@ function showResult(resp) {
 	let data = resp.data;
 
 	// data が文字列型なら，オブジェクトに変換する
-	if (typeof data === 'string') {
+	/*if (typeof data === 'string') {
 		data = JSON.parse(data);
-	}
+	}*/
+	
+	let h1a = document.querySelector('h1#a');
+	let p;
+	p = document.createElement('p');
+	p.textContent = "都市名 : " + data.name + "   最高気温 : " + data.main.temp_max + "   最低気温 : " + data.main.temp_min;
+	h1a.insertAdjacentElement('beforeend', p);
 
-	// data をコンソールに出力
-	console.log(data);
 }
 function showError(err) {
 	console.log(err);
@@ -37,6 +37,10 @@ function showError(err) {
 
 // 通信の最後にいつも実行する処理
 function finish() {
-	console.log(selected_value);
 	console.log('通信が終わりました');
+}
+
+function reset() {
+	let p = document.querySelector('p');
+	p.remove()
 }
